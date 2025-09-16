cc59d3cb39ee4feb8b04f6db03fef91a88c81ec7
Armando Alves Neto <64908110+aaneto2@users.noreply.github.com>
2025-09-16 09:47:57 -0300

    Códigos do motor Nidec

---
+++ MotorBLDC/MotorBLDC_embedded_code/MotorBLDC.h
@@ -0,0 +1,123 @@
+////////////////////////////////////////////////////////////
+// NIDEC actuator and sensor (encoder) hardware setup
+////////////////////////////////////////////////////////////
+
+#include <ESP32Encoder.h>
+#include <esp32-hal-ledc.h>
+
+// PWM config
+#define PWM_BASE_FREQ 19000
+#define PWM_RESOLUTION 8
+
+// NIDEC pins: Motor connection
+#define PWM 19 //Green wire  (PWM)
+#define BRAKE 18 //Orange wire (Start/Stop)
+#define DIR 23 //Yellow wire  (Forward/Reverse) 
+#define ENCODER_CHA 13 //Blue wire    (Signal A)  
+#define ENCODER_CHB 5 //Purple wire      (Signal B)
+#define ENCODER_PPR 400 // Pulses Per Revolution
+#define PWM_CH 1
+
+// Encoder global object
+ESP32Encoder motor_enc;
+
+// Initialize and configure measurement processes.
+void ControlSystem::setup_sensors(void) {
+  // Setup encoder.
+  ESP32Encoder::useInternalWeakPullResistors = puType::up;
+  ESP32Encoder::isrServiceCpuCore = 0;
+
+  // Configuration for the BLDC NIDEC motor.
+  ::motor_enc.attachFullQuad(ENCODER_CHA, ENCODER_CHB);
+  ::motor_enc.clearCount();
+}
+
+// Initialize and configure actuation processes.
+void ControlSystem::setup_actuators(void) {
+  // DC Brushless NIDEC motor configuration.
+  pinMode(DIR, OUTPUT);
+  pinMode(BRAKE, OUTPUT);
+
+  // Initial direction (clockwise)
+  digitalWrite(BRAKE, HIGH);
+
+  // PWM channel configuration (using the ledc library).
+  ledcSetClockSource(LEDC_AUTO_CLK);
+  ledcAttachChannel(PWM, PWM_BASE_FREQ, PWM_RESOLUTION, PWM_CH);
+
+  u[0] = 0.0f;
+  command_actuators();
+}
+
+// Actuation process.
+// Available variables: controller output vector u.
+void ControlSystem::command_actuators(void) {
+  float pwm = u[0];
+
+  if (pwm < 0) {
+    digitalWrite(DIR, HIGH);
+    pwm = -pwm;
+  } else {
+    digitalWrite(DIR, LOW);
+  }
+
+  // Saturation:
+  pwm = pwm > ((float)(1 << PWM_RESOLUTION) - 1.0f) ? ((float)(1 << PWM_RESOLUTION) - 1.0f) : pwm;
+ 
+  // NIDEC inverted PWM command:
+  pwm = 255.0f - pwm;
+
+  // PWM generation.
+  ledcWriteChannel(PWM_CH, (int)pwm);
+}
+
+// Actions when control task is about to start.
+void ControlSystem::on_start_task(void) {
+  // Reset the control system. It set internal states
+  // (controller and measurement filter states) to zero, as well as
+  // the ctrl_last_t and meas_last_t time instants.
+  reset();
+
+  // Start the encoder counter at zero.
+  motor_enc.clearCount();
+}
+
+// Actions performed when the control task is about to stop.
+void ControlSystem::on_stop_task(void) {
+  // Initialize the reference steps values to zero.
+  RefSteps.reset();
+
+  // Stop the motor.
+  u[0] = 0.0;
+  command_actuators();
+}
+
+///////////////////////////////////////////////////////////////////
+// The measurement process is implemented here.
+// Available variables (all private variables of the ControlSystem class,
+// but especially the following):
+//   t            -> time [s]
+//   meas_last_t  -> last time this code run [s].
+//   ym           -> vector of measured signals.
+//   xf           -> measurement filter state vector.
+// Measurement process.
+void ControlSystem::measure_signals(float t) {
+  float pos_motor;
+
+  // Position [deg] estimation from encoder accumulated pulses.
+  // Hypothesis: PPR pulses per turn (revolution).
+  pos_motor = motor_enc.getCount() / ((float) ENCODER_PPR)*(360.0f);
+
+  // BLDC Motor angular speed estimation in [deg/s]:
+  ym[1] = (pos_motor - ym[0]) / (t - meas_last_t);
+
+  // Update the BLDC Motor angular position measurement in [deg]:
+  ym[0] = pos_motor;
+
+  // Euler angles from the IMU.
+  ym[2] = 0.0;
+  ym[3] = 0.0;
+}
+
+// The next line will include the actual control law.
+#include "control_strategy.h"
